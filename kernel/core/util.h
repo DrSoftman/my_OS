bf69e468ab819fa02117e8fb55d485a23f3d42ec
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-28 15:39:56 +0200

    fix

--- kernel/core/util.h
+++ kernel/core/util.h
@@ -28,39 +28,6 @@ typedef u8 bool;
 
 #define NULL (0)
 
-#define CONCAT_IMPL(x, y) x##y
-#define CONCAT(x, y) CONCAT_IMPL(x, y)
-
-#define __MIN_IMPL(_x, _y, _xn, _yn) __extension__({\
-        __typeof__(_x) _xn = (_x);\
-        __typeof__(_y) _yn = (_y);\
-        (_xn < _yn ? _xn : _yn);\
-        })
-#define MIN(_x, _y) __MIN_IMPL(_x, _y, CONCAT(__x, __COUNTER__), CONCAT(__y, __COUNTER__))
-
-#define __MAX_IMPL(_x, _y, _xn, _yn) __extension__({\
-        __typeof__(_x) _xn = (_x);\
-        __typeof__(_y) _yn = (_y);\
-        (_xn > _yn ? _xn : _yn);\
-        })
-#define MAX(_x, _y) __MAX_IMPL(_x, _y, CONCAT(__x, __COUNTER__), CONCAT(__y, __COUNTER__))
-
-#define CLAMP(_x, _mi, _ma) (MAX(_mi, MIN(_x, _ma)))
-
-// returns the highest set bit of x
-// i.e. if x == 0xF, HIBIT(x) == 3 (4th index)
-// WARNING: currently only works for up to 32-bit types
-#define HIBIT(_x) (31 - __builtin_clz((_x)))
-
-// returns the lowest set bit of x
-#define LOBIT(_x)\
-    __extension__({ __typeof__(_x) __x = (_x); HIBIT(__x & -__x); })
-
-// returns _v with _n-th bit = _x
-#define BIT_SET(_v, _n, _x) __extension__({\
-        __typeof__(_v) __v = (_v);\
-        (__v ^ ((-(_x) ^ __v) & (1 << (_n))));\
-        })
 
 #define PACKED __attribute__((packed))
 
@@ -99,33 +66,7 @@ static inline size_t strlen(const char *str) {
     return l;
 }
 
-static inline char *itoa(i32 x, char *s, size_t sz) {
-    // TODO: omg this is bad code. :sob:
-    /* if (sz < 20) {
-        //panic("ITOA BUFFER TOO SMALL"); - causes a error
-    }
-    */
 
-    u32 tmp;
-    i32 i, j;
-
-    tmp = x;
-    i = 0;
-
-    do {
-        tmp = x % 10;
-        s[i++] = (tmp < 10) ? (tmp + '0') : (tmp + 'a' - 10);
-    } while (x /= 10);
-    s[i--] = 0;
-
-    for (j = 0; j < i; j++, i--) {
-        tmp = s[j];
-        s[j] = s[i];
-        s[i] = tmp;
-    }
-
-    return s;
-}
 
 static inline void memset(void *dst, u8 value, size_t n) {
     u8 *d = (u8*)dst;
@@ -162,59 +103,6 @@ static inline void *memmove(void *dst, const void *src, size_t n) {
     return dst;
 }
 
-static inline size_t strlcat(char *dst, const char *src, size_t size) {
-    const size_t sl = strlen(src),
-          dl = strlen(dst);
-
-    if (dl == size) {
-        return size + sl;
-    }
-
-    if (sl < (size - dl)) {
-        memcpy(dst + dl, src, sl + 1);
-    } else {
-        memcpy(dst + dl, src, size - dl - 1);
-        dst[size - 1] = '\0';
-    }
-
-    return sl + dl;
-}
-
-static inline void strcpy(char *dest, const char *src) {
-    while (*src) {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';  // Null-terminate the destination string
-}
-
-
-
-static inline size_t strlcpy(char *dst, const char *src, size_t n) {
-    // copy as many bytes as can fit
-    char *d = dst;
-    const char *s = src;
-    size_t size = n;
-
-    while (--n > 0) {
-        if ((*d++ = *s++) == 0) {
-            break;
-        }
-    }
-
-    // if we ran out of space, null terminate
-    if (n == 0) {
-        if (size != 0) {
-            *d = 0;
-        }
-
-        // traverse the rest of s
-        while (*s++);
-    }
-
-   return s - src - 1;
-}
 
 #ifdef __cplusplus
 }
